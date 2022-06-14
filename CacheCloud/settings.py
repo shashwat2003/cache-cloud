197956659f1fd1e37ccfa8767f44c5dd2063e8a7
shashwat2003 <info.superpcstduio@gmail.com>
2022-06-14 22:49:50 +0530

    otpapp: Configure E-Mail Settings

--- CacheCloud/settings.py
+++ CacheCloud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .key import email, password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -55,6 +56,13 @@ CORS_ALLOW_HEADERS = [
     'Access-Control-Allow-Origin',
 ]
 
+
+# Email Services
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = email
+EMAIL_HOST_PASSWORD = password
+EMAIL_USE_TLS = True
 
 # Application definition
 
--- OtpApp/views.py
+++ OtpApp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.http import HttpRequest, JsonResponse
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
+
+def generate(request: HttpRequest):
+    # send_mail(subject="Testing", message="Hello!", from_email=settings.EMAIL_HOST_USER, recipient_list=["shashwat13.8@gmail.com",])
+    return JsonResponse({"success":"Sent!"})
