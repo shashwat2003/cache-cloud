197a19ecf69c3510a79251e77f583b9c6a019e1f
shashwat2003 <info.superpcstduio@gmail.com>
2022-06-15 18:14:17 +0530

    userapp: Add role Field

--- UserApp/models.py
+++ UserApp/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    USER = 1
+    ADMIN = 2
+    ROLE_CHOICES = ((USER,"User"), (ADMIN,"Admin"))
+    
     username = models.CharField(max_length=15, unique=True, null=False)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=1)
